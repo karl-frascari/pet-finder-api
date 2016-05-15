291f73cb444c09e4861377ab5e8286bd2e247214
karlFrascari2 <karl.frascari@gmail.com>
2016-05-15 15:50:39 -0300

    Pet start

---
+++ controllers/api.js
@@ -0,0 +1,57 @@
+var _ = require('lodash');
+var async = require('async');
+
+var graph;
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+exports.getApi = function(req, res) {
+  res.render('api/index', {
+    title: 'API Examples'
+  });
+};
+
+
+exports.getFacebook = function(req, res, next) {
+  graph = require('fbgraph');
+
+  var token = _.find(req.user.tokens, { kind: 'facebook' });
+  graph.setAccessToken(token.accessToken);
+  async.parallel({
+    getMe: function(done) {
+      graph.get(req.user.facebook + "?fields=id,name,email,first_name,last_name,gender,link,locale,timezone", function(err, me) {
+        done(err, me);
+      });
+    },
+    getMyFriends: function(done) {
+      graph.get(req.user.facebook + '/friends', function(err, friends) {
+        done(err, friends.data);
+      });
+    }
+  },
+  function(err, results) {
+    if (err) {
+      return next(err);
+    }
+    res.render('api/facebook', {
+      title: 'Facebook API',
+      me: results.getMe,
+      friends: results.getMyFriends
+    });
+  });
+};
+
+
+exports.getFileUpload = function(req, res, next) {
+  res.render('api/upload', {
+    title: 'File Upload'
+  });
+};
+
+exports.postFileUpload = function(req, res, next) {
+  req.flash('success', { msg: 'File was uploaded successfully.'});
+  res.redirect('/api/upload');
+};
+
